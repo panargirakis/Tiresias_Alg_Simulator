44fac6ad3e6eb08fcc511faafb79f5b46a47242e
panargirakis <panargirakis@gmail.com>
2020-03-30 21:42:39 -0400

    init

---
+++ 2d_LAS_Sim.py
@@ -0,0 +1,47 @@
+
+# SELECT WHICH ALGORITHM TO USE:
+
+# ALGORITHM = "SRSF"
+ALGORITHM = "2D-LAS"
+
+
+def priority_SRSF(time_needed, GPUs, time_elapsed):
+    if time_needed == time_elapsed:
+        return -9999
+    return -(time_needed - time_elapsed) * GPUs
+
+def priority_2DLAS(time_needed, GPUs, time_elapsed):
+    if time_needed == time_elapsed:
+        return -9999
+    return -time_elapsed * GPUs
+
+# first value represents the number of GPUs required and the second value represents the job duration
+
+class Job:
+    def __init__(self, GPUs, time_needed):
+        self.time_needed = time_needed
+        self.GPUs = GPUs
+        self.time_elapsed = 0
+
+jobs = [Job(2,3), Job(1,4), Job(3,2), Job(2,2)]
+
+time_elapsed = 0
+jobs_finished = 0
+jcts = []
+while jobs_finished < 4:
+    priorities = []
+    for a_job in jobs:
+        if (ALGORITHM == "SRSF"):
+            priorities.append(priority_SRSF(a_job.time_needed, a_job.GPUs, a_job.time_elapsed))
+        else:
+            priorities.append(priority_2DLAS(a_job.time_needed, a_job.GPUs, a_job.time_elapsed))
+    ind_of_job_to_do = priorities.index(max(priorities))
+    print("At time: ", (time_elapsed + 1), "  Doing job: ", (ind_of_job_to_do + 1))
+    jobs[ind_of_job_to_do].time_elapsed += 1
+    time_elapsed += 1
+    if jobs[ind_of_job_to_do].time_elapsed == jobs[ind_of_job_to_do].time_needed:
+        jobs_finished += 1
+        print("\tJob ", (ind_of_job_to_do + 1), " finished!")
+        jcts.append(time_elapsed)
+print("Total time elapsed is: ", time_elapsed)
+print("Average JCT is: ", sum(jcts)/4)
