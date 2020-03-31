c3a5b797c445d0648ef20056bba01a945d4d4764
panargirakis <panargirakis@gmail.com>
2020-03-31 16:07:02 -0400

    cleanup and documentation

--- 2d_LAS_Sim.py
+++ 2d_LAS_Sim.py
@@ -5,43 +5,61 @@
 algorithm = "2D-LAS"
 
 
+# Shortest-remaining-service-first algorithm. Assumes known job completion time.
 def priority_SRSF(time_needed, n_of_gpus, time_elapsed):
-    if time_needed == time_elapsed:
-        return -9999
     return -(time_needed - time_elapsed) * n_of_gpus
 
-def priority_2DLAS(time_needed, n_of_gpus, time_elapsed):
-    if time_needed == time_elapsed:
-        return -9999
+# 2D LAS algorithm. Assumes no knowledge about the job completion time.
+def priority_2DLAS(n_of_gpus, time_elapsed):
     return -time_elapsed * n_of_gpus
 
-# first value represents the number of n_of_gpus required and the second value represents the job duration
 
+# abstraction of a job
 class Job:
-    def __init__(self, n_of_gpus, time_needed):
+    jct = None  # init to no type so error is caused if left unset
+
+    def __init__(self, n_of_gpus, time_needed, name):
         self.time_needed = time_needed
         self.n_of_gpus = n_of_gpus
+        self.name = name
         self.time_elapsed = 0
 
-jobs = [Job(2,3), Job(1,4), Job(3,2), Job(2,2)]
+    def hasFinished(self):
+        return self.time_elapsed == self.time_needed
 
-time_elapsed = 0
-jobs_finished = 0
-jcts = []
-while jobs_finished < 4:
+    def setJCT(self, jct):
+        self.jct = jct
+
+
+# Create jobs. All jobs are assumed to take at least one time unit to complete.
+jobs = [Job(2,3, "J1"), Job(1,4, "J2"), Job(3,2, "J2"), Job(2,2, "J3")]
+
+time_elapsed = 0  # keep track of time
+finished_jobs = []  # list of finished jobs
+while len(jobs):
+
+    # calculate priorities for each job
     priorities = []
     for a_job in jobs:
         if (algorithm == "SRSF"):
             priorities.append(priority_SRSF(a_job.time_needed, a_job.n_of_gpus, a_job.time_elapsed))
         else:
-            priorities.append(priority_2DLAS(a_job.time_needed, a_job.n_of_gpus, a_job.time_elapsed))
-    ind_of_job_to_do = priorities.index(max(priorities))
-    print("At time: ", (time_elapsed + 1), "  Doing job: ", (ind_of_job_to_do + 1))
-    jobs[ind_of_job_to_do].time_elapsed += 1
+            priorities.append(priority_2DLAS(a_job.n_of_gpus, a_job.time_elapsed))
+    # determine job to be done (the one with max priority)
+    job_to_do = jobs[priorities.index(max(priorities))]
+
+    print("At time: ", (time_elapsed + 1), "  Doing job: ", job_to_do.name)
+
+    # do job
     time_elapsed += 1
-    if jobs[ind_of_job_to_do].time_elapsed == jobs[ind_of_job_to_do].time_needed:
-        jobs_finished += 1
-        print("\tJob ", (ind_of_job_to_do + 1), " finished!")
-        jcts.append(time_elapsed)
+    job_to_do.time_elapsed += 1
+
+    # determine if finished and save metrics
+    if job_to_do.hasFinished():
+        print("\tJob ", job_to_do.name, " finished!")
+        job_to_do.setJCT(time_elapsed)
+        finished_jobs.append(job_to_do)  # move job from jobs to finished_jobs
+        jobs.remove(job_to_do)
+
 print("Total time elapsed is: ", time_elapsed)
-print("Average JCT is: ", sum(jcts)/4)
+print("Average JCT is: ", sum(c.jct for c in finished_jobs)/len(finished_jobs))
